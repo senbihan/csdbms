0f5032d75147192b9fbbe4bc025866e24d1d44d9
Bihan Sen <senbihan@gmail.com>
2018-01-25 11:42:12 +0530

    Upload include directory, metadata file

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # csdbms
-Class Project of ISI Kolkata
+
+**Introduction**
+This is a collaborative class project of Indian Statistical Institute, Kolkata for M.Tech CS Batch 2017-19.
+
+****
+**Change Log:**
+**Latest addition**:
+1. Common header `struct_info.h` in the include directory
+2. `Metadata.pdf`
+
+**Next to be added:**
+1. Modfied integer/real datatype.
+2. Common make file
---
+++ include/struct_info.h
@@ -0,0 +1,70 @@
+/**
+ * This is the common header file for the metadata structure
+ * All the constants and size of each field is declared as a macro
+ * 
+ * 
+ * Author(s) : Bihan Sen    (senbihan@gmail.com)
+ * 
+ * Last Modified Date : 25/01/2018
+ * 
+ **/
+
+
+#ifndef STRUCT_INFO_H
+#define STRUCT_INFO_H
+
+
+/* Constants */
+
+#define FILE_HEADER             "CS"
+
+/**
+ * Size of each fields in the metadata
+ * written in Bytes
+ **/
+
+/* Static Portion of the File */
+
+#define FILE_HEADER_SIZE        2
+#define DATE_TIME_SIZE          8   // may change
+#define RECORD_NO_SIZE          8
+#define COLUMN_NO_SIZE          1
+#define TABLE_NAME_SIZE         32
+#define RECORD_SIZE_SIZE        2
+#define DATA_HEAD_SIZE          4
+#define DATA_END_SIZE           4
+
+/* Variable Portion */
+
+#define COLUMN_DETAILS_SIZE     32
+#define COLUMN_NAME_SIZE        16
+#define DATA_TYPE_SIZE          1   
+
+// first 3-bits : data type, next 3-bits: Constraint info
+/**
+ *  Datatypes: 
+ *  int     :   000
+ *  double  :   001
+ *  string  :   010
+ *  date    :   011
+ *  time    :   100
+ * 
+ * Constriants:
+ *  primary :   000
+ *  not null:   001
+ *  auto increment: 010
+ *  no-auto-increment: 011
+ **/
+
+#define DATA_TYPE_SIZE_SIZE     1  
+
+// first 4-bits : integer part size, next 4-bit: decimal part size (Real Numbers)
+// first 4-bits : int/date/time size
+// 8 bits       : string   
+
+#define INDEX_COL_SIZE          1   // 0 : logically absent, NZ : Index of column
+#define AUTO_INC_SIZE           5
+#define MISC_SIZE               8
+
+
+#endif // STRUCT_INFO_H
